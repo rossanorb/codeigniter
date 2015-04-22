092d9e40f76baee4bc799fb8fa562700d4ba9403
Rossano <rossanorb@gmail.com>
2015-04-22 00:05:11 -0300

    exibe galeria

---
+++ system/application/views/admin/gallery.php
@@ -0,0 +1,40 @@
+<?php
+ foreach ($query as $row ){
+     if($row->tipo == 2 ){
+         //menu - pega nome da categoria do menu
+         $nome = $row->nome_categoria;
+     }else{
+         //categoria - pega nome do  menu
+         $nome = $row->menu_nome;         
+     }
+ }
+?>
+
+<div class="cols-sm-12">
+    <div class="page-header">
+      <h1><?php echo $nome ;?></h1>
+    </div>
+</div>
+<div class="cols-sm-12">
+    <ul class="row">
+    <?php 
+    foreach ($query as $row ):
+    ?>
+        <li class="col-sm-3">
+            <span class="glyphicon glyphicon-trash excluir_fotografia" id="<?php echo $row->id_fotografia; ?>"> </span>        
+            <img class="img-responsive" name="<?php echo $row->fotografia_nome; ?>" src="<?php echo FOTOS.'/'.$row->src; ?>">
+        </li>
+    <?php
+    endforeach;
+    ?>
+    </ul>
+</div>    
+    
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">         
+            <div class="modal-body">                
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
