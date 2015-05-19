cd1af77005f4a8ee4b4870debfa40ac42fadfdc5
Rossano <rossanorb@gmail.com>
2015-05-19 18:22:27 -0300

    alterado o tamanho das fotos

--- system/application/config/upload.php
+++ system/application/config/upload.php
@@ -5,7 +5,7 @@ if( $env == 'development' ){
     $config['upload'] = array(
         'upload_path' =>  './uploads/',
         'allowed_types'=> 'gif|jpg|png',
-        'max_size'=> 300,
+        'max_size'=> 400,
         'max_width'=> 966,
         'max_height' => 648,
         'overwrite' => FALSE
@@ -14,7 +14,7 @@ if( $env == 'development' ){
     $config['upload'] = array(
         'upload_path' =>  './uploads/',
         'allowed_types'=> 'gif|jpg|png',
-        'max_size'=> 300,
+        'max_size'=> 400,
         'max_width'=> 966,
         'max_height' => 648,
         'overwrite' => FALSE        
